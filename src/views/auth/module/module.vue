d146ce957e7e636f2f39d00513606fa2a61c5a61
herton7362 <he.tang@everydayratings.com>
2018-01-22 17:27:46 +0800

    编写模块管理

--- src/views/auth/module/module.vue
+++ src/views/auth/module/module.vue
@@ -1,11 +1,54 @@
+<style lang="less">
+    @import './module.less';
+</style>
+
 <template>
-    <div>
-        <h1>模块管理</h1>
+    <div class="module-main">
+        <div class="module-tree">
+            <Card>
+                <Tree :data="tree.data"></Tree>
+            </Card>
+        </div>
+        <div class="module-list">
+            <Card>
+
+            </Card>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'module'
-    };
+import util from '@/libs/util';
+import singleTable from '@/views/my-components/single-table/single-table.vue';
+
+export default {
+    name: 'module',
+    components: {
+        singleTable
+    },
+    data() {
+        return {
+            tree: {
+                raw: [],
+                data: []
+            }
+        }
+    },
+    methods: {
+        loadTreeData() {
+            util.ajax.get('/api/module', {
+                params: {
+                    sort:'sortNumber,updatedDate',
+                    order: 'asc,desc'
+                }
+            }).then((response)=>{
+                this.tree.data = util.transformTreeData(response.data.content, 'name', false);
+                this.tree.data.forEach((n)=>n.expand = true);
+            });
+        }
+    },
+    mounted() {
+        this.loadTreeData();
+    }
+};
 </script>
